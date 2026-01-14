a409ee63bea0abafa74d1bd746192a2cbe51d793
Mya <skyusmpp@gmail.com>
2026-01-14 10:23:54 -0700

    add jquery

--- student.api/obj/Debug/net9.0/student.api.AssemblyInfo.cs
+++ student.api/obj/Debug/net9.0/student.api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("student.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7df25b1b2d11e0f6685daad41e800a11e1197cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ada6866c8fdd48f014a8b945620a21c2f91a7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("student.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("student.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- student_angular20/src/app/components/student/student.ts
+++ student_angular20/src/app/components/student/student.ts
@@ -1,8 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { Student } from '../../models/student';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+// import { StudentService } from '../../services/student.service';
+
+// @Component({
+//   selector: 'app-student',
+//   standalone: true,
+//   imports: [CommonModule, FormsModule],
+//   templateUrl: './student.html',
+//   styleUrl: './student.css',
+// })
+// export class StudentComponent implements OnInit {
+//   students: Student[] = [];
+//   student: Student = {
+//     studName: '',
+//     email: '',
+//     city: '',
+//     state: '',
+//     mobileNumber: '',
+//     zipcode: '',
+//     addressLine1: '',
+//     addressLine2: ''
+
+//   };
+//   constructor(private studentService: StudentService){}
+
+//   ngOnInit(): void {
+//     this.loadStudents();
+//   }
+//   loadStudents(){
+//     this.studentService.getStudents().subscribe(data => {
+//       this.students = data;
+//     });
+//   }
+
+//   saveStudent(){
+//     console.log('Saving student:', this.student);
+//     if(this.student.studentId){
+//       this.studentService.updateStudent(this.student).subscribe(() => {
+//         this.resetForm();
+//         this.loadStudents();
+//       });
+//     }else{
+//       this.studentService.createStudent(this.student).subscribe(() =>{
+//         this.resetForm();
+//         this.loadStudents();
+//       })
+//     }
+//   }
+
+//   editStudent(s: Student){
+//     this.student = {...s };
+//   }
+//   deleteStudent(id: number){
+//     this.studentService.deleteStudent(id).subscribe(()=>{
+//       this.loadStudents();
+//     })
+//   }
+//   resetForm() {
+//     this.student = {
+//       studName: '',
+//       email: '',
+//       city: '',
+//       state: '',
+//       mobileNumber: '',
+//       zipcode: '',
+//       addressLine1: '',
+//       addressLine2: ''
+//   };
+
+// }
+// }
+
+
+
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Student } from '../../models/student';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { StudentService } from '../../services/student.service';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-student',
@@ -11,7 +89,7 @@ import { StudentService } from '../../services/student.service';
   templateUrl: './student.html',
   styleUrl: './student.css',
 })
-export class StudentComponent implements OnInit {
+export class StudentComponent implements OnInit, AfterViewInit {
   students: Student[] = [];
   student: Student = {
     studName: '',
@@ -22,42 +100,60 @@ export class StudentComponent implements OnInit {
     zipcode: '',
     addressLine1: '',
     addressLine2: ''
-
   };
-  constructor(private studentService: StudentService){}
+
+  constructor(private studentService: StudentService) {}
 
   ngOnInit(): void {
     this.loadStudents();
   }
-  loadStudents(){
+
+  ngAfterViewInit(): void {
+    $('#searchBox').on('keyup', function () {
+      const value = ($(this).val() as string).toLowerCase();
+      $('table tbody tr').each(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      });
+    });
+  }
+
+  loadStudents() {
     this.studentService.getStudents().subscribe(data => {
       this.students = data;
     });
   }
 
-  saveStudent(){
+  saveStudent() {
     console.log('Saving student:', this.student);
-    if(this.student.studentId){
+
+    if (this.student.studentId) {
       this.studentService.updateStudent(this.student).subscribe(() => {
+        const id = this.student.studentId!;
         this.resetForm();
         this.loadStudents();
+        this.highlightRow(id);
       });
-    }else{
-      this.studentService.createStudent(this.student).subscribe(() =>{
+    } else {
+      this.studentService.createStudent(this.student).subscribe((created) => {
         this.resetForm();
         this.loadStudents();
-      })
+        if (created.studentId) {
+          this.highlightRow(created.studentId);
+        }
+      });
     }
   }
 
-  editStudent(s: Student){
-    this.student = {...s };
+  editStudent(s: Student) {
+    this.student = { ...s };
   }
-  deleteStudent(id: number){
-    this.studentService.deleteStudent(id).subscribe(()=>{
+
+  deleteStudent(id: number) {
+    this.studentService.deleteStudent(id).subscribe(() => {
       this.loadStudents();
-    })
+    });
   }
+
   resetForm() {
     this.student = {
       studName: '',
@@ -68,10 +164,18 @@ export class StudentComponent implements OnInit {
       zipcode: '',
       addressLine1: '',
       addressLine2: ''
-  };
+    };
+  }
 
+  private highlightRow(studentId: number): void {
+    setTimeout(() => {
+      const row = $(`#row-${studentId}`);
+      if (row.length) {
+        row.addClass('table-warning');
+        setTimeout(() => row.removeClass('table-warning'), 1200);
+      }
+    }, 300);
+  }
 }
-}
-
 
 
